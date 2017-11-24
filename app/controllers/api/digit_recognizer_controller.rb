2e04808c96b97c65812a08fa420b3ba528c97c77
arfullight <tmimvhuang@gmail.com>
2017-11-23 18:28:12 -0800

    respond prediction using pycall to call sklearn clf

--- app/controllers/api/digit_recognizer_controller.rb
+++ app/controllers/api/digit_recognizer_controller.rb
@@ -1,4 +1,18 @@
 class Api::DigitRecognizerController < ApplicationController
+
+  # load clf on include
+  pyimport :pickle
+
+  f = File.new(
+    Rails.root.join('app', 'classifier', 'RandomForestClassifier.pkl'
+  ), 'r')
+
+  @@clf = pickle.load(f)
+  f.close
+
   def predict
+    predict = @@clf.predict([params[:digits]])
+
+    render json: { predict: "#{predict[0]}".to_i }
   end
 end
