b1d0cfdc6718e76c126746904133d6210bf9ecb3
arfullight <tmimvhuang@gmail.com>
2017-12-07 17:18:27 -0800

    draw a cross at a time

--- app/javascript/packs/pages/BlockCanvas.js
+++ app/javascript/packs/pages/BlockCanvas.js
@@ -5,21 +5,24 @@ class BlockCanvas {
   }
 
   initCanvas() {
-    let blockHtml = '<div class="block"></div>'
-
-    for (let i = 0; i < 28 * 28; i++)
-      this.element.insertAdjacentHTML('beforeend', blockHtml)
+    for (let i = 0; i < 28 * 28; i++) {
+      this.element.insertAdjacentHTML(
+        'beforeend',
+        `<div class="block" block-id="${i}"></div>`
+      )
+    }
 
     this.blocks = this.element.getElementsByClassName('block')
 
     for (let i = 0; i < this.blocks.length; i++) {
       this.blocks[i].addEventListener('mouseover', (e) => {
-        if (e.buttons == 1)
-          e.target.className += ' hovered'
+        if (e.buttons == 1) {
+          this.highlightBlocks(e.target.getAttribute('block-id'))
+        }
       })
 
       this.blocks[i].addEventListener('mousedown', (e) => {
-        e.target.className += ' hovered'
+        this.highlightBlocks(e.target.getAttribute('block-id'))
       })
     }
   }
@@ -41,6 +44,38 @@ class BlockCanvas {
     }
 
     return digits
+  }
+
+  highlightBlocks(blockId) {
+    if (typeof(blockId) === "string") {
+      blockId = parseInt(blockId)
+    }
+
+    let highlightIds = [blockId]
+
+    // not first column
+    if (blockId % 28 !== 0) {
+      highlightIds.push(blockId - 1)
+    }
+
+    // not last column
+    if (blockId % 28 !== 27) {
+      highlightIds.push(blockId + 1)
+    }
+
+    // not first row
+    if (blockId > 27) {
+      highlightIds.push(blockId - 28)
+    }
+
+    // not last row
+    if (blockId < 756) {
+      highlightIds.push(blockId + 28)
+    }
+
+    highlightIds.forEach((id) => {
+      this.blocks[id].classList += ' hovered'
+    })
   }
 }
 
